80af63766d672263ebcc7ae96513dcc5fdc61a68
Yuriichuk-Olha <olga.kantemyr@gmail.com>
2023-01-21 11:26:00 +0100

    completed 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+    const body = document.querySelector('body')
+    const btnStart = document.querySelector('button[data-start]');
+    const btnStop = document.querySelector('button[data-stop]');
+
+    let intervalId = null
+
+btnStart.addEventListener("click", showColorClick);
+btnStop.addEventListener("click", hideColorClick);
+
+
+function showColorClick(){
+    btnStop.classList.remove('disabled');
+    btnStart.classList.add('disabled');
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+     }, 1000)
+     console.log(setInterval);
+   }
+
+
+function hideColorClick(){
+    btnStart.classList.remove('disabled');
+    btnStop.classList.add('disabled');
+    clearInterval(intervalId);
+}
+
+
